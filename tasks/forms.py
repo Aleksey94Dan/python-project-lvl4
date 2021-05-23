4fa4a66ff69e72cdaad71b765cd2c0fe28f878f4
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-05-23 19:21:20 +0700

    add widget for labels

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,28 +1,32 @@
 """Description of forms for tasks."""
 
 from django.forms import ModelForm, widgets
+from django import forms
 from django.utils.translation import gettext_lazy as _
 
 from tasks.models import Tasks
+from labels.models import Labels
 
 
 class CreateTaskForm(ModelForm):
     """Tasks."""
 
+    labels = forms.ModelMultipleChoiceField(queryset=Labels.objects.all())
+
     class Meta:
         model = Tasks
-        fields = ['name', 'description', 'status', 'executor', 'labels']
+        fields = ['name', 'description', 'status', 'executor',]
         labels = {
             'name': _('Имя'),
             'description': _('Описание'),
             'status': _('Статус'),
             'executor': _('Испольнитель'),
-            'labels': _('Метки'),
         }
 
-        widgets = {
-            'labels': widgets.SelectMultiple(),
-        }
+    def __init__(self, *args, **kwargs):
+        super(CreateTaskForm, self).__init__(*args, **kwargs)
+        self['labels'].label = _('Метки')
+
 
 
 class UpdateTaskForm(CreateTaskForm):
