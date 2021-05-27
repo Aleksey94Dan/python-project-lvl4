0093ab0d9c4261ca0a70782e6c9f3c386508da63
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-05-27 21:54:39 +0700

    add forms for filter

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,12 +1,15 @@
 """Description of forms for tasks."""
 
-from django.forms import ModelForm, widgets
 from django import forms
+from django.forms import ModelForm, widgets
 from django.utils.translation import gettext_lazy as _
 
-from tasks.models import Tasks
 from labels.models import Labels
+from statuses.models import Statuses
+from tasks.models import Tasks
 
+
+BLANK_CHOICE = [('', '-----')]
 
 class TaskListSortForm(forms.Form):
     """A form for sort and search."""
@@ -14,16 +17,27 @@ class TaskListSortForm(forms.Form):
     status = forms.ChoiceField(label=_("Статус"), required=False)
     executor = forms.ChoiceField(label=_("Исполнитель"), required=False)
     labels = forms.ChoiceField(label=_("Метка"), required=False)
-    their_tasks = forms.BooleanField(
+    only_myself = forms.BooleanField(
         label=_("Только свои задачи"),
         required=False,
         initial=False,
     )
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, statuses, labels, executor, *args, **kwargs):
         """Initialize the form."""
+        self.statuses = statuses
+        self.labels = labels
+        self.executor = executor
         super().__init__(*args, **kwargs)
-
+        self.fields['status'].choices = BLANK_CHOICE + [
+            (status.pk, status.name) for status in statuses
+        ]
+        self.fields['labels'].choices = BLANK_CHOICE + [
+            (label.pk, label.name) for label in labels
+        ]
+        self.fields['executor'].choices = BLANK_CHOICE + [
+            (ex.pk, ex.get_full_name()) for ex in executor
+        ]
 
 class CreateTaskForm(ModelForm):
     """Tasks."""
@@ -51,9 +65,5 @@ class CreateTaskForm(ModelForm):
         self['labels'].label = _('Метки')
 
 
-
 class UpdateTaskForm(CreateTaskForm):
     """Update task."""
-
-
-
--- tasks/views/home.py
+++ tasks/views/home.py
@@ -1,10 +1,13 @@
 """Logic for home page of tasks"""
 
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic.list import ListView
-from tasks.forms import TaskListSortForm
 from django.views.generic.edit import FormMixin
+from django.views.generic.list import ListView
 
+from labels.models import Labels
+from statuses.models import Statuses
+from tasks.forms import TaskListSortForm
 from tasks.models import Tasks
 from user.mixins import CustomRequiredMixin
 
@@ -17,3 +20,18 @@ class TasksListView(CustomRequiredMixin, FormMixin, ListView):
     model = Tasks
     context_object_name = 'tasks'
     login_url = reverse_lazy('login')
+
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs.update({
+           'statuses': Statuses.objects.all(),
+           'labels': Labels.objects.all(),
+           'executor': User.objects.filter(is_staff=False),
+        })
+        return kwargs
+
+
+    # def get_queryset(self):
+    #     filter_dict = self.request.GET.dict()
+    #     new_context = Tasks.objects.filter(**filter_dict)
+    #     return new_context
