c9e51f21fa8d48e6da5ffa354222e13f0e7d6ace
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-05-31 00:03:55 +0700

    add filter

--- tasks/filter.py
+++ tasks/filter.py
@@ -9,6 +9,13 @@ from statuses.models import Statuses
 from tasks.models import Tasks
 from user.models import CustomUser
 
+
+def only_self(request):
+    """Show only own tasks."""
+    if request is None:
+        return User.objects.all()
+    return User.objects.filter(pk=request.user.pk)
+
 class TaskFilter(django_filters.FilterSet):
     """Filter for sorting jobs."""
 
@@ -17,17 +24,23 @@ class TaskFilter(django_filters.FilterSet):
         field_name='status',
         label='Статус',
     )
-    # label = django_filters.ModelChoiceFilter(
-    #     queryset=Labels.objects.all(),
-    #     field_name='labels',
-    #     label='Метки',
-    # )
+    label = django_filters.ModelChoiceFilter(
+        queryset=Labels.objects.all(),
+        field_name='labels',
+        label='Метки',
+    )
     executor = django_filters.ModelChoiceFilter(
         queryset=CustomUser.objects.all(),
         field_name='executor',
         label='Исполнитель',
     )
+    author = django_filters.ModelChoiceFilter(
+        queryset=only_self,
+        field_name='author',
+        label='Только свои задачи',
+        # widget=forms.CheckboxInput(),
+    )
 
     class Meta:
         model = Tasks
-        fields = ['status', 'executor', 'labels', 'author']
+        fields = ['status', 'executor', 'label',]
