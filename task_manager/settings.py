9dbc0b53ad4020304796d79284a6502775cf7eaa
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-06-06 16:18:34 +0700

    fix clossing bracket

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -143,9 +143,7 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 3,
-            },
+        'OPTIONS': {'min_length': 3},
     },
     # {
     #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
