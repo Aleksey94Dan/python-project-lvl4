4affd5f6690ea2e489a144db49fa531325c62d68
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-07-05 21:24:45 +0700

    update locale

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -167,8 +167,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'ru-ru'
 
 LANGUAGES = [
-    ('en-us', _('English')),
-    ('ru-ru', _('Russian')),
+    ('en', 'English'),
+    ('ru', 'Russian'),
 ]
 
 LOCALE_PATHS = [
@@ -182,7 +182,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 PROJECT_NAME = 'hexlet code'
 
