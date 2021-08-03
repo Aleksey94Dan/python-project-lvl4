6e8cee84c81c852a482219fd1c997e676622aa2a
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-08-03 22:22:42 +0700

    add static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -180,12 +180,11 @@ PROJECT_NAME = 'hexlet code'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
