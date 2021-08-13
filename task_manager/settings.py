a1e9f0d4cb8ca170bd8c968dcbbbc8ee41b96f65
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2021-08-13 20:37:45 +0700

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -153,8 +153,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 
-# LANGUAGE_CODE = 'ru-ru'
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
+# LANGUAGE_CODE = 'en-us'
 
 
 LANGUAGES = [
